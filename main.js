871d2e7ae537164fe4717d1dd2266ff07f6c5a14
0zz1cl13 <seniorcl13@gmail.com>
2019-12-16 16:37:10 -0400

    commit classes

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-alert('Hello');
-
-class Teste {
-    metodo() {
-        
+class List {
+    constructor(){
+        this.data = [];
     }
+
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+class TodoList extends List{
+    
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo');
 }
