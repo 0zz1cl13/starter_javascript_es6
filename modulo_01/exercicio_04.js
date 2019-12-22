b71b538439a814d24a44b61a430175b31806fc68
0zz1cl13 <seniorcl13@gmail.com>
2019-12-22 18:45:37 -0400

    exercicio_04

---
+++ modulo_01/exercicio_04.js
@@ -0,0 +1,18 @@
+const empresa = {
+  nome: 'Rocketeseat',
+  endereco: {
+    cidade: 'Rio do sul',
+    estado: 'SC'
+  }
+};
+
+// 4.1
+const { nome, endereco: { cidade, estado } } = empresa;
+console.log(nome);
+console.log(cidade);
+console.log(estado);
+// 4.2
+function mostraInfo({ nome, idade }){
+  return `${nome} tem ${idade} anos`;
+}
+console.log(mostraInfo({ nome: 'Diego', idade: 23 }));
