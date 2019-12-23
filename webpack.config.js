0b8293b79364702c0b38b2b895d89488bde9e2b7
0zz1cl13 <seniorcl13@gmail.com>
2019-12-23 19:39:22 -0400

    exercicio_modulo03

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  entry: './src/main.js', // arquivo principal com es6 >
+  entry: ['@babel/polyfill', './src/main.js'], // arquivo principal com es6 >
   output: {
     path: __dirname + '/public/',
     filename: 'bundle.js',
   },
   devServer: {
     contentBase: __dirname + '/public',
+    watchOptions: {
+      poll: true
+    },
   },
   module: {
     rules: [
